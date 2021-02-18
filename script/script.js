9dd25f350c2d23ffe103339ea1e3bb8c5b46ee5e
hakimoss <hakimos@live.ca>
2021-02-18 15:54:23 -0500

    direction droite et gauche

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+
+
 document.onmousemove = suitsouris;
 
 function suitsouris()
@@ -5,13 +7,15 @@ function suitsouris()
     if(navigator.appName == "Microsoft Internet Explorer")
     {
         var x = event.x+document.body.scrollLeft;
-        var y = event.y+document.body.scrollTop;
+
     }
     else
     {
         var x = event.pageX;
-        var y = event.pageY;
+        
     }
     document.getElementById("raquette").style.left = (x+1)+'px';
-    document.getElementById("raquette").style.top = (y+1)+'px';
+    
 }
+
+ 
