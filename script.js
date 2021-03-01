423b2d8a713ea06f4fd18f64059525fc993535d5
hakimoss <hakimos@live.ca>
2021-02-28 21:26:29 -0500

    essait page acceuille element 3d qui fait une rotation

--- script.js
+++ script.js
@@ -20,3 +20,48 @@ function randomBtnFuntion(){
         window.location.replace("https://hakimoss.github.io/random_dice/index.html")
     }
 }
+
+
+/// obj 3d
+
+
+import { GLTFLoader } from "./GLTFLoader.js";
+
+var scene = new THREE.Scene();
+
+
+var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    5000,
+);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth/1.4, window.innerHeight/1.4);
+document.body.appendChild(renderer.domElement);
+console.log(renderer.domElement)
+
+
+var loader = new GLTFLoader();
+
+var obj;
+loader.load("scene.gltf", function(gltf){
+    obj = gltf.scene;
+    scene.add(gltf.scene);
+});
+scene.background = new THREE.Color(0xffffff)
+var light = new THREE.HemisphereLight(0xffffff, 0x000000, 4);
+scene.add(light);
+camera.position.set(-5, 15, 15);
+
+
+camera.lookAt(0, 0, 0)
+function animate(){
+    requestAnimationFrame(animate);
+    obj.rotation.y += 0.01;
+    
+    renderer.render(scene, camera);
+}
+
+camera.filmOffset
+animate();
