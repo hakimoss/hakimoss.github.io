4f5909f818c07c9e92cd92ac2ea8744ebe2c8ee8
hakimoss <hakimos@live.ca>
2021-03-02 11:03:58 -0500

    ajout de random-password(n'est pas completer) et ajout d'un menu a la page d'acceuil

---
+++ random_password/script.js
@@ -0,0 +1,85 @@
+let resultat = document.getElementById("resultat");
+
+let btnRandomPassword = document.getElementById("btn_random_password");
+btnRandomPassword.addEventListener('click', functionResultat);
+
+let checkBoxcarSpeciaux = document.getElementById("caractere_speciaux").checked;
+
+
+function functionResultat(){
+
+    let nbCaractere = document.getElementById("input_number").value;
+    
+    if(checkBoxcarSpeciaux === 1){
+        chartPassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%?&*()_+-=";
+    } else {
+        chartPassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    }
+        
+
+    function functionRandomPassword(length = nbCaractere){
+
+        
+
+        let strPassword = "";       
+        let chartPasswordLength = chartPassword.length
+        
+        
+
+        for (let i = 0; i < length; i++){
+            strPassword += chartPassword.charAt(Math.floor(Math.random() * chartPasswordLength))
+        }
+       
+        return strPassword
+    }
+    
+    strPassword = functionRandomPassword();
+    resultat.textContent = strPassword;
+}
+
+
+
+
+// on va asseyer de changer la valeur par defaut en js du nombre indiquez dans le chant
+
+
+
+
+
+/*
+
+/// btn random-box
+
+let btnRandomBox = document.getElementById("btn_random_box");
+btnRandomBox.addEventListener('click', randomBtnFuntion)
+
+let final_random_box;
+
+function randomBtnFuntion(){
+    
+
+    let random_box_math = Math.floor(Math.random() * 4);
+    
+    if (random_box_math === 0){
+        window.location.replace("https://hakimoss.github.io/random_image/index.html")
+    } else if (random_box_math === 1) {
+        window.location.replace("https://hakimoss.github.io/random_color/index.html")
+    } else if (random_box_math === 2){
+        window.location.replace("https://hakimoss.github.io/random_position/index.html")
+    } else {
+        window.location.replace("https://hakimoss.github.io/random_dice/index.html")
+    }
+}
+
+// bouton retour
+
+let btnRetour = document.getElementById("btn_retour");
+btnRetour.addEventListener('click', btnRetourFunction)
+
+function btnRetourFunction(){
+    window.location.replace("https://hakimoss.github.io/")
+}
+
+
+*/
+
